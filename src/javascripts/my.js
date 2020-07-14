9d971c949a8c51dfaf50a02af815bed65a82c434
kensei <67773256+kensei-t@users.noreply.github.com>
2020-07-14 20:56:05 +0900

    Not delete dist files

--- src/javascripts/my.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-  const obj = { a: 1, b: 2 };
-  const newObj = { ...obj, c: 3 }
-  // Inject obj with '...obj' to { a: 1, b: 2, c: 3 }
-  console.log('this is module' ,newObj )
-}
